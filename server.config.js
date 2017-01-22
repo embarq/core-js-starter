f6f9967d436806d797f60088627652733b340286
embarq <embarq.dev@gmail.com>
2017-01-22 17:05:19 +0200

    Update

--- server.config.js
+++ server.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "files": ["./src/**/*.{js, html, css}"],
-  "port": process.env.PORT || 3000,
+  "port": process.env.PORT || 3215,
   "host": process.env.IP || 'localhost',
   "notify": false,
   "server": {
